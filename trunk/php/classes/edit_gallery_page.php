8031a186563ea274340c84552fc69d2a218b3201
comawill <comawill@4bf32295-e2c0-884f-b583-2924f859bbd8>
2005-09-12 20:15:15 +0000

    Added php/classes/edit_gallery_page.php for the gallery-editor, sorry it is only the class, no big content... ;-)

---
+++ trunk/php/classes/edit_gallery_page.php
@@ -0,0 +1,35 @@
+<?php
+/*****************************************************************************
+ *
+ *  file		: edit_gallery_page.php
+ *  created		: 2005-09-12
+ *  copyright		: (C) 2005 The ComaCMS-Team
+ *  email		: comacms@williblau.de
+ *
+ *****************************************************************************/
+
+/*****************************************************************************
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *****************************************************************************/
+	class Edit_Gallery_Page {
+		
+		function NewPage($page_id) {
+			
+		}
+	
+		function Save($page_id) {
+			
+		}
+		
+		function Edit($page_id) {
+			global $_SEERVER;
+			
+		}
+	}
+
+?>

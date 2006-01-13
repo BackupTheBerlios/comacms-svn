93738eb129435b68a78df8e5a4909e47fafd5825
comawill <comawill@4bf32295-e2c0-884f-b583-2924f859bbd8>
2006-01-13 21:26:40 +0000

    Created download.php

---
+++ trunk/php/download.php
@@ -0,0 +1,59 @@
+<?php
+/**
+ * @package ComaCMS
+ * @copyright (C) 2005 The ComaCMS-Team
+ */
+ #----------------------------------------------------------------------#
+ # file			: download.php					#
+ # created		: 2006-01-13					#
+ # copyright		: (C) 2005-2006 The ComaCMS-Team		#
+ # email		: comacms@williblau.de				#
+ #----------------------------------------------------------------------#
+ # This program is free software; you can redistribute it and/or modify	#
+ # it under the terms of the GNU General Public License as published by	#
+ # the Free Software Foundation; either version 2 of the License, or	#
+ # (at your option) any later version.					#
+ #----------------------------------------------------------------------#
+	/*
+	 * Usage:
+	 * download.php?file_id=$[id_of the_file]
+	 * Example:
+	 * download.php?file_id=14
+	 */
+	/**
+	 * Set a global to make sure that common.php is executet in the
+	 * only right context
+	 */
+	define('COMACMS_RUN', true);
+	// Do the things which are necessary
+	include('common.php');
+	// Load the file_id for the file which should be downloaded
+	$file_id = GetPostOrGet('file_id');
+	// Is it a numeric ID?
+	if(is_numeric($file_id)) { // It is possible that this is a real file_id
+		// Look up in the database
+		$sql = "SELECT *
+			FROM " .  DB_PREFIX . "files
+			WHERE file_id = $file_id
+			LIMIT 0,1";
+		$file_result = db_result($sql);
+		if($file = mysql_fetch_object($file_result)) { // We have found a file in the database
+			if(file_exists($file->file_path)) { // Check: exists the file also on the server?
+				// Increment the downloads-count of the file
+				$sql = "UPDATE " . DB_PREFIX . "files
+					SET file_downloads = " . ($file->file_downloads+1) . "
+					WHERE file_id = $file_id";
+				db_result($sql);
+				header('Location: '. $file->file_path);
+			}
+			else // Show error page "download not found" 
+				header('Location: special.php?page=d404');
+		}
+		else // Show error page "download not found" 
+			header('Location: special.php?page=d404');
+	}
+	else { // It is impossible
+		header('Location: index.php');
+	}
+	
+?>

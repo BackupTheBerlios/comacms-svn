e5c23402ce392ff5d453c605948f17acdbaa999a
comawill <comawill@4bf32295-e2c0-884f-b583-2924f859bbd8>
2006-03-05 16:13:28 +0000

    Added a missing space in the XHTML 1.0-strict doctype

--- trunk/php/lib/comalate/comalate.class.php
+++ trunk/php/lib/comalate/comalate.class.php
@@ -89,7 +89,7 @@
 					break;
 				case DOCTYPE_XHTML_STRICT:
 				default:
-					$this->_Doctype = '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN""http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">';
+					$this->_Doctype = '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">';	 
 					break;
 			}
  		}

ee7935c9c4ecb965bc26d34ce0e6bf9f7810b1ed
comawill <comawill@4bf32295-e2c0-884f-b583-2924f859bbd8>
2006-03-19 15:00:58 +0000

    Removed some styles

--- tags/v0.1/php/styles/mauri/menue.php
+++
@@ -1,5 +0,0 @@
-<?
-$menu_link = "\t\t\t\t<li><a href=\"[link]\" [new]>[text]</a></li>";
-$menu_link2 = "\t\t\t\t<li><a href=\"[link]\" [new]>[text]</a></li>";
-$menu_inline = "<div id=\"inlinemenu\"><img id=\"inlinemenuimage\" [image]/><br />[text]</div>";
-?>

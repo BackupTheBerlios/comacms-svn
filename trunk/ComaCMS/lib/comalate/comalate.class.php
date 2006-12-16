8414c5f85845ec07252a0beb36a5257f256ace74
comawill <comawill@4bf32295-e2c0-884f-b583-2924f859bbd8>
2006-12-16 19:38:27 +0000

    Fixed problem in PHP5 environments

--- trunk/ComaCMS/lib/comalate/comalate.class.php
+++ trunk/ComaCMS/lib/comalate/comalate.class.php
@@ -299,8 +299,8 @@
 					}
 				}
 			}
-			$this->Template = preg_replace( '/{(.+?)}/e', "\$this->_Replace('\\1')", $this->Template);
-			$this->Template = preg_replace( '/{(.+?)}/e', "\$this->_Replace('\\1')", $this->Template);
+			$this->Template = preg_replace( '/\{(.+?)\}/e', "\$this->_Replace('\\1')", $this->Template);
+			$this->Template = preg_replace( '/\\\{(.+?)\}/e', "\$this->_Replace('\\1')", $this->Template);
 			$this->Template = preg_replace("/<([A-Za-z0-9_]+)\:loop>(.+?)<\/\\1>/es", "\$this->_RepeatReplace('\\1', '\\2')", $this->Template);
 			
 			$this->Template = str_replace('<p></p>', '', $this->Template);
